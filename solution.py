1c17e312d7ba601d57c8eae830cfc3ade8c6b05a
bsegot <42400241+bsegot@users.noreply.github.com>
2020-05-08 18:01:51 +0200

    Add files via upload

--- solution.py
+++ solution.py
@@ -7,13 +7,25 @@
 
 
 class StateMachine:
-    def __init__(self):
+    def __init__(self, pac_type, special_pallet=False, special_pallet_location=[]):
         # todo add constants that define the pac
         # todo add trigger that define movement_print
         # todo add method that update at each turn and run the new elements that can trigger a state change
+        self.x = -1
+        self.y = -1
+        self.pac_type = pac_type
+        self.special_pallet = special_pallet
+        self.special_pallet_location = special_pallet_location
         self.handlers = {}
         self.startState = None
         self.endStates = []
+
+    def get_closest(self, visible_normal_pellet):
+        """get the closest pallet there is on the board from current pac"""
+
+    def special_pallet_still_exist(self):
+        """check if we still chase the big pallet"""
+
 
     def add_state(self, name, handler, end_state=0):
         name = name.upper()
@@ -106,3 +118,119 @@ if __name__== "__main__":
     m.run("Python is great")
     m.run("Python is difficult")
     m.run("Perl is ugly")
+
+    m = StateMachine()
+    m.add_state("Start", start_transitions)
+    m.add_state("chase_special_pellet", chase_special_state_transitions)
+    m.add_state("chase_normal_pellet", chase_normal_state_transitions)
+    m.set_start("Start")
+
+    m.run("Python is great")
+    m.run("Python is difficult")
+    m.run("Perl is ugly")
+
+# ####################################################################################
+# ####################################################################################
+
+
+import numpy as np
+
+
+def closest_node(node, nodes):
+    """find the closest point (euclidian) from (x,y) given a list of candidates points (nodes)"""
+    nodes = np.asarray(nodes)
+    dist_2 = np.sum((nodes - node) ** 2, axis=1)
+    return list(nodes[np.argmin(dist_2)]), np.argmin(dist_2)
+
+
+# my_pac_list = [{'pac_id': 0, 'mine': True, 'x': 17, 'y': 5, 'type_id': 'NEUTRAL', 'speed_turns_left': 0, 'ability_cooldown': 0}, {'pac_id': 1, 'mine': True, 'x': 27, 'y': 7, 'type_id': 'NEUTRAL', 'speed_turns_left': 0, 'ability_cooldown': 0}]
+# visible_special_pellet = [[15, 7], [19, 7], [8, 13]]
+
+def allocate_pacs(visible_special_pellet, pac_list):
+    """allocate pacs according to the closest to special pallets"""
+
+    allocation_list = []
+    for pellet in visible_special_pellet:
+
+        if len(pac_list) == 0:
+            break
+        node = [pellet[0], pellet[1]]
+        nodes = [[x['x'], x['y']] for x in pac_list]
+
+        best_pac_coordonate, pac_index = closest_node(node, nodes)
+        allocation_list.append(
+            {'pac_coordonate': best_pac_coordonate, 'pac_id': pac_list[pac_index]['pac_id'], 'special_pellet': node})
+
+        del pac_list[pac_index]
+
+    return allocation_list
+
+
+import sys
+import math
+
+# Grab the pellets as fast as you can!
+
+# width: size of the grid
+# height: top left corner is (x=0, y=0)
+width, height = [int(i) for i in input().split()]
+for i in range(height):
+    row = input()  # one line of the grid: space " " is floor, pound "#" is wall
+
+# game loop
+while True:
+    my_score, opponent_score = [int(i) for i in input().split()]
+    visible_pac_count = int(input())  # all your pacs and enemy pacs in sight
+    my_pac_list = []
+    opponent_pac_list = []
+    for i in range(visible_pac_count):
+        # pac_id: pac number (unique within a team)
+        # mine: true if this pac is yours
+        # x: position in the grid
+        # y: position in the grid
+        # type_id: unused in wood leagues
+        # speed_turns_left: unused in wood leagues
+        # ability_cooldown: unused in wood leagues
+        pac_id, mine, x, y, type_id, speed_turns_left, ability_cooldown = input().split()
+        pac_id = int(pac_id)
+        mine = mine != "0"
+        x = int(x)
+        y = int(y)
+        speed_turns_left = int(speed_turns_left)
+        ability_cooldown = int(ability_cooldown)
+        if mine == True:
+            my_pac_list.append({'pac_id': pac_id, 'mine': mine, 'x': x, 'y': y, 'type_id': type_id,
+                                'speed_turns_left': speed_turns_left, 'ability_cooldown': ability_cooldown})
+        elif mine == False:
+            opponent_pac_list.append({'pac_id': pac_id, 'mine': mine, 'x': x, 'y': y, 'type_id': type_id,
+                                      'speed_turns_left': speed_turns_left, 'ability_cooldown': ability_cooldown})
+
+            # print(my_pac_list, file=sys.stderr)
+
+    visible_pellet_count = int(input())  # all pellets in sight
+    visible_normal_pellet = []
+    visible_special_pellet = []
+    for i in range(visible_pellet_count):
+        # value: amount of points this pellet is worth
+        x, y, value = [int(j) for j in input().split()]
+        if value == 1:
+            visible_normal_pellet.append([x, y])
+        elif value == 10:
+            visible_special_pellet.append([x, y])
+
+    # print(visible_special_pellet, file=sys.stderr)
+
+    # Write an action using print
+    # To debug: print("Debug messages...", file=sys.stderr)
+
+    # MOVE <pacId> <x> <y>
+    test = allocate_pacs(visible_special_pellet, my_pac_list)
+
+    char_print = ''
+    for orders in test:
+        if char_print == '':
+            char_print = f"MOVE {orders['pac_id']} {orders['special_pellet'][0]} {orders['special_pellet'][1]}"
+        else:
+            char_print += f" | MOVE {orders['pac_id']} {orders['special_pellet'][0]} {orders['special_pellet'][1]}"
+
+    print(char_print)
